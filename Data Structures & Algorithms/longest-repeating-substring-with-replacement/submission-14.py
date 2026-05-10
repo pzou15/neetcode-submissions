8d913aaa3dd8d60c39b920a4896b4e383865bf30
Phillip Zou <84814375+pzou15@users.noreply.github.com>
2026-05-10 09:41:12 -0600

    Add: longest-repeating-substring-with-replacement - submission-14

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-14.py
@@ -0,0 +1,39 @@
+from collections import defaultdict
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        freqMap = defaultdict(int) # char : count
+        maxLen = 0
+        l = 0
+        # I'm dumb part 2
+        # basically the main thing to know is that k = number of characters in a substring that don't match the majority char
+        # if number of chars that don't match majority char > k then it's invalid, this problem doens't care about WHICH char or substring
+        # so we can probably just keep a count of what the majority is -> if r - l + 1 - majorityCount > k update left
+
+        for r,c in enumerate(s):
+            # get majority char
+            freqMap[c] += 1
+            mostFrequentC = max(freqMap, key=freqMap.get)
+            count = freqMap[mostFrequentC]
+            # update left pointer against majority char
+            while r - l + 1 - count > k:
+                freqMap[s[l]] -= 1
+                l += 1
+            maxLen = max(maxLen, r - l + 1)
+        
+        return maxLen
+
+            
+                
+
+                
+                             
+            
+            
+
+
+
+
+    
+
+
+
