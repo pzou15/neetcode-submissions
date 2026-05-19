77130cd4bc7f449475727c63283d243c6b436f7b
Phillip Zou <84814375+pzou15@users.noreply.github.com>
2026-05-19 12:16:49 -0600

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.py
@@ -0,0 +1,47 @@
+import string
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        depth = len(word)
+        for c in word:
+            cur.depth = max(cur.depth, depth)
+            depth -= 1
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.word = True
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+        return self.searchRecurse(word, cur)
+        
+    def searchRecurse(self, word, root):
+        cur = root
+        for i in range(len(word)):
+            if word[i] == '.':
+                for c in string.ascii_lowercase:
+                    if c not in cur.children:
+                        continue
+                    tail = ""
+                    if i + 1 < len(word):
+                        tail = word[i+1:]
+                    remaining = c + tail
+                    if len(remaining) > cur.depth:
+                        continue
+                    found = self.searchRecurse(remaining, cur)
+                    if found:
+                        return True
+            if word[i] not in cur.children:
+                return False
+            cur = cur.children[word[i]]
+        return cur.word
+        
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = False
+        self.depth = 0
